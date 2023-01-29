669b4e5e03c13f43d0d1762c81fdbb9f0e8b1ce2
AartiJakhar <aartijakharkct@gmail.com>
2023-01-29 13:59:29 +0530

    updating status

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
       <div className="container mx-auto flex px-16 py-20 lg:flex-row flex-col items-center">
         <div className="lg:flex-grow lg:w-1/2 lg:pr-24  flex flex-col lg:items-start lg:text-left mb-16  items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Aarti Jakhar a frotend developer.
+            Hi, I'm Aarti Jakhar a full stack  developer.
           </h1>
           <p className="mb-4 leading-relaxed">
             I have started learning programming after 12th by myself . almost a
